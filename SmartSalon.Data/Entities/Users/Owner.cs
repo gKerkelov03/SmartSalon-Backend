353c80067dda2cd4c553efac2b88c9405b643fec
Georgi Kerkelov <gkerkelov03@abv.bg>
2024-03-19 11:52:03 +0200

    remove the Entities folder from the Data layer

--- SmartSalon.Data/Entities/Users/Owner.cs
+++
@@ -1,8 +0,0 @@
-﻿using SmartSalon.Data.Entities.Salons;
-
-namespace SmartSalon.Data.Entities.Users;
-
-public class Owner : UserWithProfile
-{
-    public virtual IList<Salon>? Salons { get; set; }
-}
