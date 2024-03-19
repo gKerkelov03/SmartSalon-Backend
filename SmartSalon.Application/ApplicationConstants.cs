e170fa92f8a95bb094973f547b4cc68b8f151e30
Georgi Kerkelov <gkerkelov03@abv.bg>
2024-03-19 11:19:35 +0200

    Turn the services layer into an application layer

---
+++ SmartSalon.Application/ApplicationConstants.cs
@@ -0,0 +1,10 @@
+namespace SmartSalon.Services;
+
+public static class ApplicationConstants
+{
+    public const string IdNotFoundError = "This id does not exist";
+
+    public const string EmailNotFoundError = "This email does not exist";
+
+    public const string IncorrectPasswordError = "This password is incorrect";
+}
