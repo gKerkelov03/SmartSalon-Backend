2bc76957626b445404d1b63adbbf808b976e7830
Georgi Kerkelov <gkerkelov03@abv.bg>
2024-04-01 13:26:19 +0300

    add properties to an api response model

--- SmartSalon.Presentation.Web/Features/Users/Responses/GetUserByIdResponse.cs
+++ SmartSalon.Presentation.Web/Features/Users/Responses/GetUserByIdResponse.cs
@@ -6,8 +6,14 @@ namespace SmartSalon.Presentation.Web.Features.Users.Responses;
 
 public class GetUserByIdResponse : IMapFrom<GetUserByIdQueryResponse>
 {
-    public required string Name { get; set; }
+    public required string UserName { get; set; }
 
-    public required int Age { get; set; }
+    public required string FirstName { get; set; }
+
+    public required string LastName { get; set; }
+
+    public required string Email { get; set; }
+
+    public required string PictureUrl { get; set; }
 }
 
